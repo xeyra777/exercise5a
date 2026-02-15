7607bb4cfb4137438707ba0fc6cabad4d36f0f00
xeyra <Xeyra.anza@gmail.com>
2026-02-15 22:15:24 +0700

    refactor#2 - add paceCal fn.

--- common.js
+++ common.js
@@ -8,6 +8,15 @@ function validatePace(pace) {
 		return { "status": 0, "message": msg, "hour": 0, "min": 0 };
 	}
 }
+function paceCal(pace,dist) {
+	var min = dist * pace;
+	var hour = 0;
+	if (min > 60) {
+		hour = Math.floor(min / 60);
+		min = min % 60;
+	}
+	return {"min":min,"hour":hour};
+}
 function calculate(p, d) {
 	var msg = "";
 	var pace = parseFloat(p);
@@ -16,12 +25,7 @@ function calculate(p, d) {
 	var chk = validatePace(pace);
 	if(chk != null) return chk;
 	//---------------------------
-	var min = dist * pace;
-	var hour = 0;
-	if (min > 60) {
-		hour = Math.floor(min / 60);
-		min = min % 60;
-	}
+	
 	//---------------------------
 	msg = "successfully calculated!"
 	return { "status": 1, "message": msg, "hour": hour, "min": min };
